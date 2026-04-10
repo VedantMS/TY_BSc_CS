3e797e962ab4e8cbd8be76e2008e7952868e3a2a
VedantMS <vedantsalunke1234@gmail.com>
2026-04-10 10:51:06 +0530

    Database Metadata

---
+++ Java/JDBC/database_info.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+class database_info {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tybsccs", "postgres", "godspell");
+            Statement st = conn.createStatement();
+
+            DatabaseMetaData dbmd = conn.getMetaData();
+
+            System.out.println("Database Name: " + dbmd.getDatabaseProductName());
+            System.out.println("Database Version: " + dbmd.getDatabaseProductVersion());
+            System.out.println("Driver Name: " + dbmd.getDriverName());
+            System.out.println("Driver Version: " + dbmd.getDriverVersion());
+
+            String[] table = {"TABLE"};
+            ResultSet rs = dbmd.getTables(null, null, null, table);
+
+            System.out.println("\nTables: ");
+
+            while(rs.next()) {
+                System.out.println(rs.getString(3));
+            }
+        }
+
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
